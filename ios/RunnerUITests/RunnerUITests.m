a84f8a7250bfb630796590a16a9fd4a5ac4d1e7c
keigomichi <k.keigomichi@gmail.com>
2025-02-26 13:57:42 +0900

    test: add patrol for iOS

---
+++ ios/RunnerUITests/RunnerUITests.m
@@ -0,0 +1,12 @@
+//
+//  RunnerUITests.m
+//  RunnerUITests
+//
+//  Created by 加藤恵吾 on 2025/02/26.
+//
+
+@import XCTest;
+@import patrol;
+@import ObjectiveC.runtime;
+
+PATROL_INTEGRATION_TEST_IOS_RUNNER(RunnerUITests)
